2d2252d6201b3871cf2663ca71ed71881d28c576
tcdw <admin@tcdw.net>
2017-05-26 14:55:34 +0800

    rm jquery and opt dir

---
+++ static/werttin/index.js
@@ -0,0 +1,34 @@
+/* @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache 2.0 */
+timeSince = function(date) {
+    var interval, seconds;
+    seconds = Math.floor((new Date() - date) / 1000);
+    interval = Math.floor(seconds / 31536000);
+    if (interval > 1) {
+        return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " mins ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+};
+
+var dateItem = document.getElementsByClassName("from");
+for (var i in dateItem) {
+    dateItem[i].innerHTML = timeSince(new Date(dateItem[i].innerHTML).getTime());
+}
+
+hljs.initHighlightingOnLoad();
+/* @license-end */
