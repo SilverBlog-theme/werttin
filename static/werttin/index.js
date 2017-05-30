505f15c8b9856505696a5193bb599b18e40c649c
tcdw <admin@tcdw.net>
2017-05-30 21:37:37 +0800

    fixed a bug caused NaN time in page

--- static/werttin/index.js
+++ static/werttin/index.js
@@ -1,7 +1,7 @@
 /* @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache 2.0 */
 timeSince = function(date) {
     var interval, seconds;
-    seconds = Math.floor((new Date() - date) / 1000);
+    seconds = Math.floor((new Date().getTime() - date) / 1000);
     interval = Math.floor(seconds / 31536000);
     if (interval > 1) {
         return interval + " years ago";
@@ -27,7 +27,9 @@ timeSince = function(date) {
 
 var dateItem = document.getElementsByClassName("from");
 for (var i in dateItem) {
-    dateItem[i].innerHTML = timeSince(new Date(dateItem[i].innerHTML).getTime());
+    if (dateItem[i].innerHTML != "") {
+        dateItem[i].innerHTML = timeSince(new Date(dateItem[i].innerHTML).getTime());
+    }
 }
 
 hljs.initHighlightingOnLoad();
