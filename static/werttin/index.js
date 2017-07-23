54f47fde673253e6c99eb36f681ced4de234e6c9
tcdw <admin@tcdw.net>
2017-07-23 23:41:41 +0800

    重写了 timeSince 函数

--- static/werttin/index.js
+++ static/werttin/index.js
@@ -1,29 +1,27 @@
 /* @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache 2.0 */
-timeSince = function(date) {
-    var interval, seconds;
-    seconds = Math.floor((new Date().getTime() - date) / 1000);
-    interval = Math.floor(seconds / 31536000);
-    if (interval > 1) {
-        return interval + " years ago";
+var gapType = [
+    {max: 60, suffix: " seconds ago"},
+    {max: 3600, suffix: " minutes ago"},
+    {max: 86400, suffix: " hours ago"},
+    {max: 604800, suffix: " days ago"},
+    {max: 2592000, suffix: " weeks ago"},
+    {max: 31536000, suffix: " months ago"}
+];
+
+timeSince = function (date) {
+    var gap = (new Date().getTime() - date) / 1000;
+    var i = 0;
+    while (true) {
+        if (gap >= ((i <= 0) ? 0 : gapType[i - 1].max) && gap < gapType[i].max) {
+            return Math.floor(gap / ((i <= 0) ? 1 : gapType[i - 1].max)) + gapType[i].suffix;
+        } else if (gap > gapType[gapType.length - 1].max || gap < 0) {
+            var ds = new Date(date).toDateString().split(" ");
+            return ds[1] + " " + ds[2] + ", " + ds[3];
+        } else {
+            i += 1;
+        }
     }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months ago";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days ago";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours ago";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " mins ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-};
+}
 
 var dateItem = document.getElementsByClassName("date");
 for (var i in dateItem) {
