8f6772d07096e1721be7644a5e9f9013cc323fd5
tcdw <admin@tcdw.net>
2017-07-25 16:58:41 +0800

    修复了导致 hljs 不工作的问题

--- static/werttin/index.js
+++ static/werttin/index.js
@@ -14,7 +14,7 @@ timeSince = function (date) {
     while (true) {
         if (gap >= ((i <= 0) ? 0 : gapType[i - 1].max) && gap < gapType[i].max) {
             return Math.floor(gap / ((i <= 0) ? 1 : gapType[i - 1].max)) + gapType[i].suffix;
-        } else if (gap > gapType[gapType.length - 1].max || gap < 0) {
+        } else if (gap > gapType[gapType.length - 1].max || gap < 0 || i >= gapType.length) {
             var ds = new Date(date).toDateString().split(" ");
             return ds[1] + " " + ds[2] + ", " + ds[3];
         } else {
