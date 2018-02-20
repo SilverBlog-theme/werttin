0fe41fcc6f46dd32972625989c4bd0bb228a160f
jianghua huang <qwe7002@hotmail.com>
2018-02-21 00:27:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 templates_name="werttin"
+
 if [ $(basename `pwd`) != "templates" ];then
     echo "[Error] Please do this in the templates directory!"
     exit
 fi
 if [ ! -d ${templates_name} ]; then
-    git clone https://github.com/SilverBlogTeam/${templates_name}.git
+    git clone https://github.com/SilverBlogTheme/${templates_name}.git
 fi
 ln -sv ../${templates_name}/static ./static/${templates_name}
 cd ${templates_name}
-if [ -f "config.json" ]; then
+if [ -f "config.example.json" ]; then
     cp config.example.json config.json
-    vim config.json
 fi
